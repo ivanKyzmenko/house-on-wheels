97afe54e1bd37683a2bb0389ef3039b57a9f07d0
Ivan Kyzmenko <ivan.kyzmenko@icloud.com>
2019-09-17 10:26:02 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <main class="app">
+
+      <main-header></main-header>
+
+      <router-view class="app-content"></router-view>
+
+      <main-footer></main-footer>
+
+    </main>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import MainHeader from '@/components/MainHeader.vue'
+import MainFooter from '@/components/MainFooter.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MainHeader,
+    MainFooter
   }
 }
+</script>
+
+<style lang="scss">
+  @import 'styles/main.scss';
+  * {
+    font-family: 'Montserrat Regular', sans-serif;
+  }
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    &-content {
+      padding-top: 178px;
+      flex-grow: 2;
+      height: 1200px;
+      background: lightgrey;
+    }
+  }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,102 @@
+<template>
+  <header class="app-header">
+    <div class="app-header__logo">
+      <div class="app-header__logo-inside">
+        <img src="http://addison.bold-themes.com/main-demo/wp-content/uploads/sites/3/2017/05/Addison-logo-3.png"
+             alt="logo" title="addison">
+      </div>
+    </div>
+    <div class="app-header__menu" v-bind:class="{'app-header__menu-fixed' : menuFixedState}">
+      <div class="app-header__menu-inside">
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">Home</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+            <li class="app-header__menu-inside__item-list__item app-header__menu-inside__item-list__item-sub">
+              <a class="app-header__menu-inside__item-list__item-link" href="">sub menu</a>
+
+              <ul class="app-header__menu-inside__item-list__item-sub__list">
+                <li class="app-header__menu-inside__item-list__item-sub__list-item">
+                  <a href="" class="app-header__menu-inside__item-list__item-sub__list-item__link">sub menu link #1</a>
+                </li>
+                <li class="app-header__menu-inside__item-list__item-sub__list-item">
+                  <a href="" class="app-header__menu-inside__item-list__item-sub__list-item__link">sub menu link #2</a>
+                </li>
+              </ul>
+
+            </li>
+          </ul>
+        </div>
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">Gallery</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+          </ul>
+        </div>
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">Plans</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+          </ul>
+        </div>
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">Financing</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+          </ul>
+        </div>
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">Invertory</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+          </ul>
+        </div>
+        <div class="app-header__menu-inside__item">
+          <a class="app-header__menu-inside__item-title">FAQ</a>
+          <ul class="app-header__menu-inside__item-list">
+            <li class="app-header__menu-inside__item-list__item">
+              <a class="app-header__menu-inside__item-list__item-link" href="">regular menu</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  components: {
+  },
+  data: () => ({
+    menu: null,
+    submenu: null,
+    menuFixedState: false
+  }),
+  mounted () {
+    window.addEventListener('scroll', this.scrollHandler)
+  },
+  methods: {
+    scrollHandler () {
+      this.menuFixedState = window.scrollY > 96
+    }
+  }
+}
+</script>
